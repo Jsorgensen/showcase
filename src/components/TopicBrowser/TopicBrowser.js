e19dbd56469982dbe98f10132431ecb2279f6944
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-11-30 12:12:24 -0700

    EvenAndOdd works

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -3,7 +3,14 @@ import React, {Component} from 'react';
 class TopicBrowser extends Component{
     render(){
         return(
-            <p>Hello Computer!</p>
+            <div>
+                <h1>Topics</h1>
+                <a >Even and Odd</a>
+                <a>Filter Object</a>
+                <a>Filter String</a>
+                <a>Palindrome</a>
+                <a>Sum</a>
+            </div>
         )
     }
 }
--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -3,8 +3,38 @@ import React, {Component} from 'react';
 class EvenAndOdd extends Component{
     render(){
         return(
-            <p>Even And Odd</p>
+            <div className='puzzleBox evenAndOddPB'>
+                <h4>Evens and Odds</h4>
+                <input className='inputLine' onChange={(e)=>this.updateUserInput(e.target.value)}></input>
+                <button className='confirmationButton' onClick={()=>this.solveToyProblem()}></button>
+                <span className='resultsBox'>Evens: {this.state.evenArray.join(", ")}</span>
+                <span className='resultsBox'>Odds: {this.state.oddArray.join(", ")}</span>
+            </div>
         )
+    }
+    constructor(){
+        super();
+        this.state = ({
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        })
+    }
+    updateUserInput(val){
+        this.setState({userInput: val});
+    }
+    solveToyProblem(){
+        var arr = this.state.userInput.split(',');
+        var even = [];
+        var odd = [];
+
+        even = arr.filter(num => num % 2 === 0);
+        odd = arr.filter(num => num % 2 !== 0);
+
+        this.setState({
+            evenArray: even,
+            oddArray: odd
+        })
     }
 }
 
